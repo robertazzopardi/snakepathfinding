96d786bce354eca82301341119a93dff32b80051
robertazzopardi <robertazzopardi@icloud.com>
2020-09-22 20:21:09 +0100

    refactored find_path method in Astar method to improve code metrics

--- src/snake/algorithms/astar.py
+++ src/snake/algorithms/astar.py
@@ -5,55 +5,57 @@ from collections import deque
 class AStar:
     NAME = "Astar"
 
-    @staticmethod
-    def find_path(start, end, graph):
-        G = {}
-        F = {}
+    def find_path(self, start, end, graph):
+        self.G = {}
+        self.F = {}
 
-        G[start] = 0
-        F[start] = graph.heuristic(start, end)
+        self.G[start] = 0
+        self.F[start] = graph.heuristic(start, end)
 
-        closed_vertices = set()
-        open_vertices = set([start])
-        came_from = {}
+        self.closed_vertices = set()
+        self.open_vertices = set([start])
+        self.came_from = {}
 
-        while len(open_vertices) > 0:
+        while len(self.open_vertices) > 0:
             current = None
             current_fscore = None
-            for pos in open_vertices:
-                if current is None or F[pos] < current_fscore:
-                    current_fscore = F[pos]
+            for pos in self.open_vertices:
+                if current is None or self.F[pos] < current_fscore:
+                    current_fscore = self.F[pos]
                     current = pos
 
             if current == end:
                 path = [current]
-                while current in came_from:
-                    current = came_from[current]
+                while current in self.came_from:
+                    current = self.came_from[current]
                     path.append(current)
                 path.reverse()
 
                 return path[1:]
 
-            open_vertices.remove(current)
-            closed_vertices.add(current)
+            self.open_vertices.remove(current)
+            self.closed_vertices.add(current)
 
             x, y = current
+            self.check_state(end, graph, x, y, current)
 
-            for x2, y2 in graph.adjacent_edges(x, y):
-                neighbour = (x2, y2)
-                if neighbour in closed_vertices:
+    def check_state(self, end, graph, x, y, current):
+        for x2, y2 in graph.adjacent_edges(x, y):
+            neighbour = (x2, y2)
+            if neighbour in self.closed_vertices:
+                continue
+
+            candidate_g = self.G[current] + graph.move_cost(neighbour)
+
+            try:
+                if neighbour not in self.open_vertices and graph.board[y2][x2] != (SNAKE_BODY or SNAKE_HEAD):
+                    self.open_vertices.add(neighbour)
+                elif candidate_g >= self.G[neighbour]:
                     continue
-                candidate_g = G[current] + graph.move_cost(neighbour)
+            except Exception:
+                pass
 
-                try:
-                    if neighbour not in open_vertices and graph.board[y2][x2] != (SNAKE_BODY or SNAKE_HEAD):
-                        open_vertices.add(neighbour)
-                    elif candidate_g >= G[neighbour]:
-                        continue
-                except Exception:
-                    pass
-
-                came_from[neighbour] = current
-                G[neighbour] = candidate_g
-                H = graph.heuristic(neighbour, end)
-                F[neighbour] = G[neighbour] + H
+            self.came_from[neighbour] = current
+            self.G[neighbour] = candidate_g
+            H = graph.heuristic(neighbour, end)
+            self.F[neighbour] = self.G[neighbour] + H
--- src/snake/pathfinder.py
+++ src/snake/pathfinder.py
@@ -6,10 +6,14 @@ class Pathfinder(AStar, Bfs):
     def __init__(self, start, end, graph, algorithm):
         self.name = algorithm
 
+        self.astar = AStar()
+        self.bfs = Bfs()
+
         self.finder = {
-            AStar.NAME: lambda x, y, z: AStar.find_path(x, y, z),
-            Bfs.NAME: lambda x, y, z: Bfs.find_path(x, y, z)
+            AStar.NAME: lambda x, y, z: self.astar.find_path(x, y, z),
+            Bfs.NAME: lambda x, y, z: self.bfs.find_path(x, y, z)
         }
+
         self.path = self.finder[self.name](start, end, graph)
 
     def get_path(self, start, end, graph):
